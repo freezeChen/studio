a314696feb6bfe11c67fb16559ac4e210c4514bc
frozen <wancsc@163.com>
2019-07-06 23:02:32 +0800

    ä¿ä¿fix 配置ææ模板

--- template/cmd.go
+++ template/cmd.go
@@ -11,23 +11,23 @@ name: "{{.Appname}}"
 env: "dev"
 version: "v0.0.1"
 mysql:
-  source: "test:test@tcp(172.16.0.148:3306)/bilibili_show?timeout=5s&readTimeout=5s&writeTimeout=5s&parseTime=true&loc=Local&charset=utf8,utf8mb4"
-  active: 100
-  idle: 20
+	source: "test:test@tcp(172.16.0.148:3306)/bilibili_show?timeout=5s&readTimeout=5s&writeTimeout=5s&parseTime=true&loc=Local&charset=utf8,utf8mb4"
+	active: 100
+	idle: 20
 log:
-	name: {{.Appname}}
+	name: "{{.Appname}}"
 	debug: true
 	LogFileDir: "./log/"
 	KafkaAddr: "127.0.0.1:9092"
 	WriteKafka: false
 redis:
-  addr: "127.0.0.1:6379"
-  auth: ""
-  active: 100
-  idle: 20
-  dialTimeout: "1ms"
-  readTimeout: "1ms"
-  writeTimeout: "1ms"
-  idleTimeout: "1ms"
+	addr: "127.0.0.1:6379"
+	auth: ""
+	active: 100
+	idle: 20
+	dialTimeout: "1ms"
+	readTimeout: "1ms"
+	writeTimeout: "1ms"
+	idleTimeout: "1ms"
 kafka: "127.0.0.1:9092"
 `
